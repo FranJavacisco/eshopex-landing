1a3c10215ece314123560e0a43b85268c1e4fb7a
Francisco Lopez <lopezf28@gmail.com>
2024-12-04 00:56:21 -0300

    Configuración para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   base: '/eshopex-landing/',
-  plugins: [react()],
+  plugins: [react({
+    jsxRuntime: 'classic' 
+  })],
   server: {
     port: 3000
   },
   build: {
     outDir: 'dist',
-    sourcemap: true,
-    commonjsOptions: {
-      include: []
-    }
+    sourcemap: true
   }
 })
